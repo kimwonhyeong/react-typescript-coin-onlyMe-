2035c49683507b1a42c83f124c454bae4bf0ac3e
kimwonhyeong <ansun667@naver.com>
2022-10-30 05:38:47 +0000

    ghpages100

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Router from './Router';
 import { ReactQueryDevtools } from "react-query/devtools";
 import {HelmetProvider} from "react-helmet-async";
 const GlobalStyle = createGlobalStyle`
-	@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 		html, body, div, span, applet, object, iframe,
 	h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 	a, abbr, acronym, address, big, cite, code,
@@ -25,12 +24,6 @@ const GlobalStyle = createGlobalStyle`
 	  font: inherit;
 	  vertical-align: baseline;
 	}
-	/* HTML5 display-role reset for older browsers */
-	article, aside, details, figcaption, figure,
-	footer, header, hgroup, main, menu, nav, section {
-	  display: block;
-	}
-	/* HTML5 hidden-attribute fix for newer browsers */
 	*[hidden] {
 		display: none;
 	}
